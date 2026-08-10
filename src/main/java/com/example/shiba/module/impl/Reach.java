1ae92ec5a608a23d1c503a8ac0e510913736da1b
shiba <137372530+Shineverthinkforit019@users.noreply.github.com>
2026-08-11 02:14:04 +0700

    Update Reach.java

--- src/main/java/com/example/shiba/module/impl/Reach.java
+++ src/main/java/com/example/shiba/module/impl/Reach.java
@@ -5,10 +5,10 @@ import com.example.shiba.module.Category;
 import com.example.shiba.module.settings.NumberSetting;
 
 public class Reach extends Module {
-    public final NumberSetting range = new NumberSetting("Range", 3.0, 4.5, 3.5, 0.05);
+    public final NumberSetting range = new NumberSetting("Range", 3.0, 6.0, 3.5, 0.05);
 
     public Reach() {
-        super("Reach", "Tăng khoảng cách tấn công (legit)", Category.COMBAT);
+        super("Reach", "Tăng khoảng cách tấn công", Category.COMBAT);
     }
 
     public double getReach() {
